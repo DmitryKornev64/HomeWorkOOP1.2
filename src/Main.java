a3e86ef3b62aa6043796d71a2deddb7d60ab64d8
dmitry <dmitrykornev.64@gmail.com>
2022-10-18 05:08:47 +0300

    ООП ДЗ 3, Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-// дз 2
-        car ladaGrande = new car("Lada", "Grande", 2015, "желтого", "России", "1.7");
-        car audiA8 = new car("Audi", "A8 50 L TDI quattro", 2020, "черный", "Германии", "3.0");
-        car bMVZ8 = new car("BMW", "Z8", 2021, "черный", "Германии", "3.0");
-        car kiaSportage4 = new car("Kia", "Sportage 4 поколение", 2018, "красный", "Южной Корее", "2.4");
-        car hyundaiAvante = new car("Hyundai", "Avante", 2016, "оранжевый", "Южной Корее", "1.6");
+        // дз 2
+        //Car ladaGrande = new Car("Lada", "Grande", 2015, "желтого", "России", 1.7);
+        Car ladaGrande = new Car(null, null, 0, null, null, 0);
+        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2020, "черный", "Германии", 3.0);
+        Car bMVZ8   = new Car("BMW", "Z8", 2021, "черный", "Германии", 3.0);
+        Car kiaSportage4 = new Car("Kia", "Sportage 4 поколение", 2018, "красный", "Южной Корее", 2.4);
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, "оранжевый", "Южной Корее", 1.6);
         ladaGrande.carInfo();
         audiA8.carInfo();
         bMVZ8.carInfo();
